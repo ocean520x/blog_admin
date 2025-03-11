bcd08da21190f9fd1585fd7ba042b584463fd3af
Ocean <ocean_520x@bytespark.app>
2025-03-11 14:08:27 +0800

    fix:  修复更新头像接口报错的问题

--- app/Services/UploadService.php
+++ app/Services/UploadService.php
@@ -10,10 +10,10 @@ class UploadService
     public function image($file, $width = 800, $height = 800, $fit = Fit::Contain)
     {
         // attachments/xxxx.png
-        $filePath = $file->store('public/attachments');
+        $filePath = $file->store('attachments', 'public');
         // storage/app/attachments/xxxx.png
-        $realPath = storage_path('app/' . $filePath);
+        $realPath = storage_path('app/public/' . $filePath);
         Image::load($realPath)->fit($fit, $width, $height)->save();
-        return url('storage/' . str_replace('public/', '', $filePath));
+        return url('storage/' . $filePath);
     }
 }
